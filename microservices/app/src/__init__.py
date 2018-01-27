71061d3c81db85208d52b6a71597deb0452aa015
anujanegi <anujanegi1@gmail.com>
2018-01-27 20:43:33 +0530

    api blueprint removed

--- microservices/app/src/__init__.py
+++ microservices/app/src/__init__.py
@@ -1,11 +1,5 @@
-from flask import Flask, Blueprint, current_app, request, json, jsonify
+from flask import Flask
 
 app = Flask(__name__)
-api = Blueprint('api',__name__)
-
-@api.before_request
-def before_request():
-    if request.json is None:
-        return jsonify(code=400, message="JSON parameters expected"), 400
 
 from .server import *
--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -4,12 +4,16 @@ from src import app
 def home():
     return "Intercom Analysis - T12PF1"
 
-@api.route('/getconversation', methods=['POST'])
+@app.route('/getconversation', methods=['POST'])
 def getconversation():
     days = request.json.get("days", None)
     hours = request.json.get("hours", None)
     minutes = request.json.get("minutes", None)
     time = request.json.get("time", None)
     emailid = request.json.get("emailid", None)
+
+    if request.json is None:
+        return jsonify(code=400, message="JSON parameters expected"), 400
+
 
     return days
