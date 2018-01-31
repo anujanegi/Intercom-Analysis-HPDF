f8fca50ca4be13268e838189a52df1c433ca7e20
anujanegi <anujanegi1@gmail.com>
2018-02-01 00:01:43 +0530

    opne_time calculaion

--- microservices/app/src/timestamp.py
+++ microservices/app/src/timestamp.py
@@ -1,2 +1,7 @@
+import time
+
 def minus_time(days, hours, minutes):
     return ((days*86400) + (hours*3600) + (minutes*60))
+
+def calculate_open_time(mentioned_time):
+    return (int(time.time()) - int(mentioned_time))
