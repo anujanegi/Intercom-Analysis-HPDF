875bf82132c5c3af5e59d5d22e7a3f86da518bf5
anujanegi <anujanegi1@gmail.com>
2018-02-01 01:08:29 +0530

    Stupid errors fixed

--- microservices/app/src/intercomconfig.py
+++
@@ -1,6 +0,0 @@
-from intercom.client import Client
-
-# enter the extended token require for list conversation feature on intercom
-def configure_client():
-    intercom = Client(personal_access_token='extended_access_token_here')
-    return intercom
