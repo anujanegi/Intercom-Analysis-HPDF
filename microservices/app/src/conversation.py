0229d8a82b6b16b15a6ed0b84e7d9360ffc96c88
anujanegi <anujanegi1@gmail.com>
2018-01-31 23:50:27 +0530

    filtering conversations added

--- microservices/app/src/conversation.py
+++ microservices/app/src/conversation.py
@@ -1,9 +1,16 @@
 from intercom.client import Client
 
 def getconversations(intercom, open_time):
-    filtered_convo = []
+    open_convo = []
 	for convo in intercom.conversations.find_all(type='conversation_message', open= True):
-        	convo_dict = convo.to_dict()
-        	if(int(convo_dict['created_at']) > int(open_time)):
-                filtered_convo.append(convo_dict)
+            #appendinf list of open converstions with dictionary of convo object of intercom.conversation.Conversation
+            open_convo.append(convo.to_dict())
+    return open_convo
+
+def filterconversations(conversationlist, open_time):
+    filtered _convo = []
+    for convo in conversationlist:
+        if(int(convo['created_at']) > int(open_time)):
+            # append to filtered_convo if open for greater than open_time
+            filtered_convo.append(convo)
     return filtered_convo
