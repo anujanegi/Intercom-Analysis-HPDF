3a010c9eb5d2d80e0e8ceadfc3ba3dd79791c5dd
anujanegi <anujanegi1@gmail.com>
2018-02-01 02:39:32 +0530

    sender bug fixed

--- microservices/app/response_sender.py
+++ microservices/app/response_sender.py
@@ -4,7 +4,7 @@ def bad_request(message):
     return jsonify(code=400, message=message), 400
 
 def Error(code = 500, message):
-    return jsonify(code=code message=message), code
+    return jsonify(code=code, message=message), code
 
 def OK():
     return jsonify(code=200, message="Email sent!"), 200
