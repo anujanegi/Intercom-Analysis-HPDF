bf7c209b96f125d28eeb5c9179bfe0c2314f851b
anujanegi <anujanegi1@gmail.com>
2018-02-01 01:17:14 +0530

    indent exception error fixed

--- microservices/app/conversation.py
+++ microservices/app/conversation.py
@@ -2,8 +2,8 @@ from intercom.client import Client
 
 def getconversations(intercom):
     open_convo = []
-	for convo in intercom.conversations.find_all(type='conversation_message', open= True):
-        # appendinf list of open converstions with dictionary of convo object of intercom.conversation.Conversation
+    for convo in intercom.conversations.find_all(type='conversation_message', open=True):
+        # appending list of open converstions with dictionary of convo object of intercom.conversation.Conversation
         open_convo.append(convo.to_dict())
     return open_convo
 
