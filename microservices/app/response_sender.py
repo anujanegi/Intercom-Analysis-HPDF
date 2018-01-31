9059883f090a3889c9625d230d7125b6a531663c
anujanegi <anujanegi1@gmail.com>
2018-02-01 02:43:50 +0530

    sender updates

--- microservices/app/response_sender.py
+++ microservices/app/response_sender.py
@@ -3,7 +3,9 @@ from flask import jsonify
 def bad_request(message):
     return jsonify(code=400, message=message), 400
 
-def Error(code = 500, message):
+def Error(code = None, message):
+    if code is None:
+        return jsonify(code=500, message=message), 500
     return jsonify(code=code, message=message), code
 
 def OK():
