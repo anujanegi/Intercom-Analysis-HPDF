17d5bc9de44bf481ce9463a4fb89a04cc1d3e3f5
anujanegi <anujanegi1@gmail.com>
2018-02-01 02:31:55 +0530

    email utitlity

--- microservices/app/email.py
+++ microservices/app/email.py
@@ -14,13 +14,31 @@ def config_email():
 
     mail.init_app(app)
 
+def getattributes(conversation):
+    #parse to get id and message body for each conversation
+    repls = ('</p>', ''), ('<p>', '')
+    msg_id = conversation['id']
+    msg_body = (conversation['conversation_message'].to_dict())['body']
+    msg_body = reduce(lambda a, kv: a.replace(*kv), repls, msg_body)
+    body = "ID: %s\tMESSAGE: %s\n" % (msg_id,msg_body)
+    return body
+
+def getbody(conversationlist):
+    # get bosy of the email from the conversations
+    body = ""
+    for convo in conversationlist:
+        body += getattributes(convo)
+    return body
+
 def send_email(body, emailid):
     try:
+        config_email()
         mail.connect()
     except Exception as e:
         Error(111, str(e))
 
     msg = Message(subject="Open conversations", body=body, sender=ADMINS[0], recipients=emailid)
+
     try:
         mail.send(msg)
     except Exception as e:
