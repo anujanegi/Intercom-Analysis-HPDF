2a3f9839a2208fca9665ede9d2b02f0793e876c0
anujanegi <anujanegi1@gmail.com>
2018-02-01 01:11:01 +0530

    Stupid errors fixed

--- microservices/app/conversation.py
+++ microservices/app/conversation.py
@@ -4,7 +4,7 @@ def getconversations(intercom):
     open_convo = []
 	for convo in intercom.conversations.find_all(type='conversation_message', open= True):
             # appendinf list of open converstions with dictionary of convo object of intercom.conversation.Conversation
-            open_convo.append(convo.to_dict())
+        open_convo.append(convo.to_dict())
     return open_convo
 
 def filterconversations(conversationlist, open_time):
