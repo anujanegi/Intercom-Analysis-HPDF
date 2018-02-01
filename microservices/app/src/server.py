637dfdd7d8a1286c039c79ee9eeaa1358b2953c7
anujanegi <anujanegi1@gmail.com>
2018-02-01 23:51:45 +0530

    API endpoint updated

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -12,28 +12,32 @@ def maillistofconversations():
     days = request.json.get("days", None)
     hours = request.json.get("hours", None)
     minutes = request.json.get("minutes", None)
-    time = request.json.get("time", None)
+    time_hour = request.json.get("time_hour", None)
+    time_minute = request.json.get("time_minute", None)
     emailid = request.json.get("emailid", None)
 
-    try:
-        # configure intercom with your extended access token
-        # edit intercomconfig.py with your token
-        intercom = configure_client()
+    def daily_email():
+        try:
+            # configure intercom with your extended access token
+            # edit intercomconfig.py with your token
+            intercom = configure_client()
 
-        # get open conversations
-        open_convo = getconversations(intercom)
+            # get open conversations
+            open_convo = getconversations(intercom)
 
-        # calculate open time acceptable as per user
-        open_time = calculate_open_time(minus_time(days, hours, minutes))
+            # calculate open time acceptable as per user
+            open_time = calculate_open_time(minus_time(days, hours, minutes))
 
-        # get filtered conversations
-        filter_convo = filterconversations(open_convo, open_time)
+            # get filtered conversations
+            filter_convo = filterconversations(open_convo, open_time)
 
-        # get body and send email
-        send_email(getbody(filter_convo), emailid)
+            # get body and send email
+            send_email(getbody(filter_convo), emailid)
 
-    except Exception as e:
-        Error(500, str(e))
+        except Exception as e:
+            Error(500, str(e))
+
+    schedule(time_hour, time_minute, daily_email)
 
     return OK()
 
