3ef45824c106fe13c8850c6d73c96b9904d38936
anujanegi <anujanegi1@gmail.com>
2018-02-04 01:06:29 +0530

    test

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -19,31 +19,32 @@ def mailconversations():
     # get seconds at which the schedule needs to be triggered
     seconds = getseconds(time_hour, time_minute)
 
-    def daily_email():
-        try:
-            # configure intercom with your extended access token
-            # edit intercomconfig.py with your token
-            intercom = configure_client()
+    #def daily_email():
+    #    try:
+    # configure intercom with your extended access token
+    # edit intercomconfig.py with your token
+    intercom = configure_client()
 
-            # get open conversations
-            open_convo = getconversations(intercom)
+    # get open conversations
+    open_convo = getconversations(intercom)
 
-            # calculate open time acceptable as per user
-            open_time = calculate_open_time(minus_time(int(days), int(hours), int(minutes)))
+    # calculate open time acceptable as per user
+    open_time = calculate_open_time(minus_time(int(days), int(hours), int(minutes)))
 
-            # get filtered conversations
-            filter_convo = filterconversations(open_convo, open_time)
+    # get filtered conversations
+    filter_convo = filterconversations(open_convo, open_time)
 
-            # get body and send email
-            send_email(getbody(filter_convo), emailid)
+    # get body and send email
+    send_email(getbody(filter_convo), emailid)
 
-        except Exception as e:
+"""    except Exception as e:
             Error(500, str(e))
     daily_email()
     #schedule(seconds, daily_email)
 
     return OK()
-
+"""
+    return intercom
 @app.route('/test')
 def test():
     return current_app.send_static_file('test.html')
